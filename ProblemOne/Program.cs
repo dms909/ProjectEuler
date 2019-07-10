c1a4078fa350612c63551b37e70d10fe15580b69
Smith, Dylan <dysmith@hy-vee.com>
2019-07-10 09:42:23 -0500

    First Commit - problem 1

---
+++ ProblemOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int final = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    final += i;
+                }
+            }
+            
+            Console.WriteLine(final);
+        }
+    }
+}
